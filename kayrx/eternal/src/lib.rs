f7eb0462f78397e31a04e65db87307f4a1b29714
krircc <krircc@aliyun.com>
2020-06-03 13:15:16 +0800

    add eternal

---
+++ kayrx/eternal/src/lib.rs
@@ -0,0 +1,38 @@
+#![warn(
+    rust_2018_idioms,
+    unreachable_pub,
+    // missing_debug_implementations,
+    // missing_docs,
+)]
+#![allow(
+    type_alias_bounds,
+    clippy::type_complexity,
+    clippy::borrow_interior_mutable_const,
+    clippy::needless_doctest_main,
+    clippy::too_many_arguments,
+    clippy::new_without_default
+)]
+
+#[macro_use]
+extern crate log;
+
+// pub use ntex_rt_macros::{main, test};
+
+pub(crate) mod task;
+pub(crate) mod testing;
+pub(crate) mod util;
+mod server;
+
+pub use self::server::*;
+
+pub(crate) mod rt {
+    pub use ntex_rt::*;
+}
+
+pub(crate) mod service {
+    pub use ntex_service::*;
+}
+
+pub(crate) mod codec {
+    pub use ntex_codec::*;
+}
